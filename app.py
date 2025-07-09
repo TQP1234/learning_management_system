b35a9989ee4c25c55dbfe92db9eaa9474be9cff8
Tan Qi Peng <tqp1234@gmail.com>
2025-07-09 21:00:38 +0800

    Update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = create_app()
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
-        port=8081,
+        port=5000,
         # ssl_context='adhoc',
         debug=False,
     )
